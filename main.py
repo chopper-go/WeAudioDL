51c292b5b5ae7856637543d125831456282deb8a
chopper-go <67249107+chopper-go@users.noreply.github.com>
2021-07-04 23:47:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 # 作者【乔巴】： https://github.com/chopper-go
+
 import requests
 import re #正则
 import time 
@@ -7,12 +8,12 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-saveFolder='G:\\mp3\\有声小说'
-pagetxt='G:\\pagetxt.txt' #调试用
+saveFolder='D:\\有声小说'
+pagetxt='D:\\pagetxt.txt' #调试用
 
 
 # 广播剧列表
-allUrl='https://mp.weixin.qq.com/mp/appmsgalbum?__biz=Mzk0OTI0NDgwOA==&action=getalbum&album_id=1864379555835101185&scene=173&from_msgid=2247488225&from_itemidx=2&count=10&nolastread=1'
+allUrl='https://mp.weixin.qq.com/mp/appmsgalbum?__biz=Mzk0OTI0NDgwOA==&action=getalbum&album_id=1864379555835101185&scene=173'
 
 
 def getMediaEachPage(url):
@@ -29,13 +30,11 @@ def getMediaEachPage(url):
         driver.close()
         Id=re.findall("voice_encode_fileid=\"(.*?)\"",pageSource)
         Name=re.findall("role=\"link\">(.*?)</strong>",pageSource)
-        l=len(Id)
-
-        if (len(Id) == 0 or len(Name) == 0):
-            print('获取不到音频信息,当前网页存在', pagetxt)
-            with open(pagetxt,'w',encoding='utf-8') as f:
-                f.write(pageSource)
-            exit()
+        # l=len(Id)
+        # if (len(Id) == 0 or len(Name) == 0):
+        #     print('获取不到音频信息,当前网页存在', pagetxt)
+        #     with open(pagetxt,'w',encoding='utf-8') as f:
+        #         f.write(pageSource)
         media=[Id,Name]
         return media
     except Exception as e:
@@ -44,7 +43,6 @@ def getMediaEachPage(url):
 def SaveMP3(media):
     mediaId=media[0]
     MediaName=media[1]
-    
     for i in range(0,len(mediaId)):
         savelink='https://res.wx.qq.com/voice/getvoice?mediaid='+mediaId[i]
         fileName=saveFolder + "\\" + MediaName[i]
@@ -52,7 +50,6 @@ def SaveMP3(media):
         with open(fileName, 'wb') as f:
             f.write(file.content)
         print(str(i) + '.'  + '已保存'+ fileName)
-
 
 def getList(url):
     chrome_options = Options()  
@@ -85,6 +82,8 @@ pageLink=allpages[1]
 for i in range(0,len(pageName)):    
     if input('是否下载 ' + pageName[i] + ' ? (Y/N) ')  =='Y':
         media=getMediaEachPage(pageLink[i])
-        SaveMP3(media)
-        print('='*6 ,pageName[i],'下载完成','='*6)
-
+        if len(media[1]) > 0:
+            SaveMP3(media)
+            print('='*6 ,pageName[i],'下载完成','='*6)
+        else:
+            print('/'*6 ,pageName[i],'跳过','/'*6)
